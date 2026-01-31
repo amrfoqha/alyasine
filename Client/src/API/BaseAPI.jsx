6d6021c7c4147b430064654be76f47f671be7dba
Amr Foqha <amr.mofeed.foqha@gmail.com>
2026-01-31 20:16:36 +0200

    last commit

--- Client/src/API/BaseAPI.jsx
+++ Client/src/API/BaseAPI.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BaseAPI = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "https://alyasine.onrender.com/api",
 });
 
 export default BaseAPI;
